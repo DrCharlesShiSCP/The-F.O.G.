9e09c3bf9fa7e0928902d61b8aa0a2f6fae97295
XxX_Charles_SCP_xXx <40179368+DrCharlesShiSCP@users.noreply.github.com>
2021-10-07 21:57:18 -0700

    Add files via upload

---
+++ Assets/scenetooffice.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenetooffice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("tooffice", 5f);
+    }
+    void tooffice()
+    {
+        SceneManager.LoadScene("OFFICE");
+    }
+}
